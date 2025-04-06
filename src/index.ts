bad593ca578e9c6477ea6d344a94199a6e624ae2
BenRyc4203 <fishs4203@gmail.com>
2025-04-06 12:46:58 +1000

    start fix

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,6 @@ import dotenv from "dotenv";
 import loginRouter from "./routes/login.ts";
 import queryRouter from "./routes/query.ts";
 import subRouter from "./routes/sub.ts";
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { S3Client } from "@aws-sdk/client-s3";
-import session from "express-session";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import { jwtSecret } from "./constants.ts";
@@ -13,7 +10,7 @@ import { jwtSecret } from "./constants.ts";
 dotenv.config();
 
 const app: express.Express = express.default();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(jwtSecret))
